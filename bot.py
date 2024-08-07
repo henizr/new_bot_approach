84706032f0f18697ec2568a817942dde992236fa
Harry <henry.iz@yandex.ru>
2024-08-07 18:42:42 +0300

    Добавил первый обработчик

--- bot.py
+++ bot.py
@@ -1,3 +1,19 @@
 from telebot import TeleBot
+from tokens import TOKEN
 
-TOKEN = ""
+bot = TeleBot(token=TOKEN)
+
+
+
+@bot.message_handler(commands=["start"])
+def start(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет, напиши 'Новая задача' для добавления новой задачи "
+        "или 'Список дел' для просмотра твоего списка"
+    )
+
+
+
+
+bot.polling(non_stop=True, interval=0)
