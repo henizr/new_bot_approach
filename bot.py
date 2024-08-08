0091947890eb04f94eb4fbdc351310214e29a99d
Harry <henry.iz@yandex.ru>
2024-08-09 01:04:44 +0300

    Добавлена обработка ввода задачи и выдача списка задач

--- bot.py
+++ bot.py
@@ -4,8 +4,51 @@ from telebot import types
 
 bot = TeleBot(token=TOKEN)
 
-BUTTONS_TITLES = ["Новая задача", "Список дел"]
+BUTTONS_TITLES = ["новая задача", "список дел"]
 
+todo_list: list = []
+
+def add_new_task(message):
+    bot.send_message(message.chat.id, "Введите имя новой задачи: ")
+
+def show_todo_list(message):
+    if todo_list:
+        for i, task in enumerate(todo_list):
+            bot.send_message(
+                message.chat.id,
+                f"{i}. {task.name}"
+                )
+        else:
+            bot.send_message(
+                message.chat.id,
+                "Список пуст"
+                )
+class Task:
+    def __init__(self, name, day=None, time=None) -> None:
+        self.name = name
+        self.day = day
+        self.time = time
+
+
+
+@bot.message_handler(commands=["newtask"])
+def new_task(message):
+    add_new_task(message=message)
+
+@bot.message_handler(commands=["showlist"])
+def show_list(message):
+    show_todo_list(message=message)
+
+@bot.message_handler(commands=["help"])
+def help(message):
+    bot.send_message(
+        message.chat.id,
+        "Привет! Вот список доступных команд:\n"
+        "/start - начало работы\n"
+        "/help - список доступных команд\n"
+        "/newtask - добавить новую задачу\n"
+        "/showlist - посмотреть список дел"
+        )
 
 # обработчик, срабатывающий на команду start
 @bot.message_handler(commands=["start"])
@@ -26,15 +69,16 @@ def start(message):
 @bot.message_handler(content_types=["text"])
 def answer(message):
     if message.text.lower() == BUTTONS_TITLES[0]:
-        bot.send_message(
-            message.chat.id,
-            "Введи название задачи: "
-        )
+        add_new_task(message=message)
     elif message.text.lower() == BUTTONS_TITLES[1]:
+        show_todo_list(message=message)
+    else:
+        new_task = Task(name=message.text)
+        todo_list.append(new_task)
+
         bot.send_message(
             message.chat.id,
-            "Здесь будет твой список дел"
+            f"Добавлена задача\n{new_task.name}"
         )
-
 
 bot.polling(non_stop=True, interval=0)
